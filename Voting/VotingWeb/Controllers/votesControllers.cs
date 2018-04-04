e57ee13385ecc9537b42de99e01354f792ea3ceb
HarshITBizRuntime <harsh.r@bizruntime.net>
2018-04-04 07:13:09 -0700

    Service Fabric Application

---
+++ Voting/VotingWeb/Controllers/votesControllers.cs
@@ -0,0 +1,30 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+namespace VotingWeb.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Votes")]
+    public class votesControllers : Controller
+    {
+        private readonly HttpClient httpClient;
+
+        public votesControllers(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        // GET: api/Votes
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<KeyValuePair<string, int>> votes = new List<KeyValuePair<string, int>>();
+            votes.Add(new KeyValuePair<string, int>("Pizza", 3));
+            votes.Add(new KeyValuePair<string, int>("Ice cream", 4));
+
+            return Json(votes);
+        }
+    }
+}
