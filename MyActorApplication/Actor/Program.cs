4563f186ed2e61ad5d198dd865d1e390ae46408a
HarshITBizRuntime <harsh.r@bizruntime.net>
2018-04-05 22:43:11 -0700

    Service Fabric Actor

---
+++ MyActorApplication/Actor/Program.cs
@@ -0,0 +1,19 @@
+﻿using HelloWorld.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Actor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHelloWorld actor = ActorProxy.Create<IHelloWorld>(ActorId.CreateRandom(), new Uri("fabric:/MyActorApplication/HelloWorldActorService"));
+            Task<string> retval = actor.GetHelloWorldAsync();
+            Console.Write(retval.Result);
+            Console.ReadLine();
+        }
+    }
+}
